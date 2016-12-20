e0eb06414ca0adf14db02a144025c1d5f6cbb741
Stephen <raphael.appsoluteperfection@gmail.com>
2016-12-19 22:42:49 -0600

    Commit a basic consumer that peeks then consumes webtask messages

---
+++ index.js
@@ -0,0 +1,34 @@
+(function(){
+    'use strict';
+
+    const unirest = require('unirest');
+    const cron = require('cron');
+
+    exports.run = (webtaskUrl, deviceToListenFor, action) => {
+        if (webtaskUrl) {
+            var cronJob = cron.job('*/5 * * * * *', 
+                () => {
+                    unirest.get(webtaskUrl + '&peek=1') // peek first to not consume others
+                    .end(response => {
+                        if (response.status === 200){
+                            let message = response.body;
+                            if (message.device === deviceToListenFor) {
+                                unirest.get(webtaskUrl)
+                                .end(response => {
+                                    if (response.status === 200){
+                                        let message = response.body;
+                                        if (message.device === deviceToListenFor) {
+                                            action();
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    });
+            });
+            cronJob.start();
+        }
+    };
+
+    
+})();
